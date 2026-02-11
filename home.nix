5f930bfbcea6256a00f68f30101385b48ae394bd
0xLogN <nutdriver716@gmail.com>
2026-02-11 16:51:17 -0500

    fix

--- home.nix
+++ home.nix
@@ -17,7 +17,7 @@
       zpkgs.libzr
       git-absorb
     ]
-    ++ (if pkgs.stdenv.isLinux then [ pmtpkgs.pipemix_tools ] else [ ]);
+    ++ (if pkgs.stdenv.isLinux then [ pmtpkgs.pipemix_tools discord prismlauncher firefox ] else [ ]);
 
   home.shellAliases = {
     # Git
