afbf9e3cad33c0340925af066935eb67c134c314
Logan Devine <logan@zirco.dev>
2026-08-08 20:04:34 -0700

    nix/nix: fix package

--- modules/nix/nix [Lld]/nix.nix
+++ modules/nix/nix [Lld]/nix.nix
@@ -9,9 +9,12 @@
     };
   };
 
-  flake.modules.homeManager.nix = {
-    nix.extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
+  flake.modules.homeManager.nix = { pkgs, ... }: {
+    nix = {
+      package = pkgs.nix;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
   };
 }
