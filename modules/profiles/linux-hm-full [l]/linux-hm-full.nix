3e2bc949271f06a8ac27dfc6518e2c370601ee14
Logan Devine <logan@zirco.dev>
2026-08-20 10:15:03 -0700

    programs/cider-2: remove, merge into profiles/linux-hm-full

--- modules/profiles/linux-hm-full [l]/linux-hm-full.nix
+++ modules/profiles/linux-hm-full [l]/linux-hm-full.nix
@@ -1,9 +1,13 @@
 { inputs, ... }:
 {
-  flake.modules.homeManager.profile-linux-hm-full = {
+  flake.modules.homeManager.profile-linux-hm-full = { pkgs, ... }: {
     imports = with inputs.self.modules.homeManager; [
       profile-common-hm-full
       pipemix
+    ];
+
+    home.packages = with pkgs; [
+      cider-2
     ];
   };
 }
--- modules/programs/cider-2 [l]/cider-2.nix
+++
@@ -1,5 +0,0 @@
-{ inputs, ... }: {
-  flake.modules.homeManager.cider-2 = { pkgs, ... }: {
-    home.packages = with pkgs; [ cider-2 ];
-  };
-}
