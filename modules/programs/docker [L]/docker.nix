fc1c8282653b1e04a63e29f83fad4d669e3f9c27
Logan Devine <logan@zirco.dev>
2026-08-22 10:19:01 -0700

    docker: add distrobox

--- modules/programs/docker [L]/docker.nix
+++ modules/programs/docker [L]/docker.nix
@@ -1,6 +1,10 @@
 { inputs, ... }: {
-  flake.modules.nixos.docker = { ... }: {
+  flake.modules.nixos.docker = { pkgs, ... }: {
     virtualisation.docker.enable = true;
     users.users.logn.extraGroups = [ "docker" ];
+    environment.systemPackages = with pkgs; [
+      distrobox
+      docker-compose
+    ];
   };
 }
